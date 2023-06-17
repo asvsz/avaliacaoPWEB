b6b31113e08bd388b0457a74f7df42b4cf4e9901
Amanda Souza <amandasldsouza@gmail.com>
2023-06-17 14:07:38 -0300

    On branch main  Changes to be committed: 	modified:   app.py 	modified:   templates/base.html 	modified:   templates/menu.html 	modified:   templates/paginas/adicionar.html 	modified:   templates/paginas/editar.html 	modified:   templates/paginas/editar_lista.html 	modified:   templates/paginas/excluir_lista.html 	modified:   templates/paginas/index.html 	modified:   templates/paginas/lista.html

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
 tarefas = []
 
-#Rota da Página Inicial
+# Rota da Página Inicial
 @app.route('/')
 def index():
     return render_template('paginas/index.html')
 
-#Rota para listar todas as Tarefas
+# Rota para listar todas as Tarefas
 @app.route('/lista')
 def lista():
-    return render_template('paginas/lista.html', tarefas=tarefas)
+    tarefaConcluida = True
+    return render_template('paginas/lista.html', tarefas=tarefas, tarefaConcluida=tarefaConcluida)
 
-#Rota para Adicionar Tarefas
+# Rota para Adicionar Tarefas
 @app.route('/adicionar', methods=['GET', 'POST'])
 def adicionar():
     if request.method == 'POST':
@@ -25,42 +26,35 @@ def adicionar():
     else:
         return render_template('paginas/adicionar.html')
 
-#Rota para escolher qual Tarefa Editar
+# Rota para escolher qual Tarefa Editar
 @app.route('/editar')
 def editarLista():
-    return render_template('paginas/editar_lista.html', tarefas=tarefas)  
+    return render_template('paginas/editar_lista.html', tarefas=tarefas)
 
-#Rota para editar a Tarefa Específica
-@app.route('/editar/<int:id>', methods=['GET','POST'])
+# Rota para editar a Tarefa Específica
+@app.route('/editar/<int:id>', methods=['GET', 'POST'])
 def editarTarefa(id):
     tarefa = next((tarefa for tarefa in tarefas if tarefa['id'] == id), None)
 
     if tarefa:
-        if request.method in ['POST', 'PUT']:
-        # Verifica se o campo oculto method contém o valor PUT
-            if request.form.get('method') == 'PUT':
-                tarefa['nome'] = request.form['nome']
-                return redirect('/editar')
+        if request.method in ['POST']:
+            tarefa['nome'] = request.form['nome']
+            return redirect('/editar')
         return render_template('paginas/editar.html', tarefa=tarefa)
     return 'Tarefa não encontrada'
 
-#Rota para escolher qual Tarefa Excluir
+# Rota para escolher qual Tarefa Excluir
 @app.route('/excluir')
 def excluirLista():
-    return render_template('paginas/excluir_lista.html', tarefas=tarefas)  
+    return render_template('paginas/excluir_lista.html', tarefas=tarefas)
 
-#Rota para excluir a Tarefa Específica
-@app.route('/excluir/<int:id>', methods=['POST'])
+# Rota para excluir a Tarefa Específica
+@app.route('/excluir/<int:id>', methods=['GET', 'POST'])
 def excluirTarefa(id):
     
-    if len(tarefas) != 0:
-    # Verifica se o campo oculto method contém o valor DELETE
-      if request.form['method'] == 'DELETE':
-        del tarefas[id]
-        return redirect('/excluir')
-        
-    else:
-        return redirect('/excluir')
+    del tarefas[id]
+    return redirect('/excluir')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
